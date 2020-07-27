cf10f1885833def46fad8565041ba8e90fad928a
Piotr Jankowski <uphobia@ymail.com>
2020-07-27 13:54:55 +0200

    Refactoring

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -16,27 +16,28 @@ class OrdersTableSeeder extends Seeder
     public function run()
     {
         // Group vehicles by aviability in branch
-        $branches = App\Branch::all();
-
-        foreach ($branches as $branch){
+        App\Branch::all()->each(function($branch){
             // Create orders untill about 10 vechicles left unassigned
             while(
                 App\Vehicle::where('branch_id',$branch->id)
                 ->whereNotIn('id', DB::table('order_vehicle')->pluck('vehicle_id'))
                 ->count()>2){
+
                     $order = new App\Order;
                     $order->employee()->associate(App\Employee::inRandomOrder()->first());
                     $order->client()->associate(App\Client::inRandomOrder()->first());
                     $order->branch()->associate($branch);
+
                     // Get random vehicles from branch
                     $vehicles = App\Vehicle::inRandomOrder()->where('branch_id',$branch->id)
                     ->whereNotIn('id', DB::table('order_vehicle')->pluck('vehicle_id'))
                     ->limit(rand(1,5))->get();
+
                     // Caluclate order cost
                     $order->cost = $vehicles->sum('cost');
                     $order->save();
                     $order->vehicles()->saveMany($vehicles);
-            }
-        }
+                }
+        });
     }
 }
